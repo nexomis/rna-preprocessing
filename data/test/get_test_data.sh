31617ae0ec17dd807824fe9f1ee960611d7170c8
jfouret <julien@fouret.me>
2025-02-07 14:00:15 +0100

    fix test gen

--- data/test/get_test_data.sh
+++ data/test/get_test_data.sh
@@ -4,7 +4,7 @@ rm -rf tmp/
 gzip ggal/gut_*
 apptainer run docker://quay.io/biocontainers/slimfastq:2.04--h503566f_5 slimfastq ggal/liver_1.fq ggal/liver_1.sfq
 apptainer run docker://quay.io/biocontainers/slimfastq:2.04--h503566f_5 slimfastq ggal/liver_2.fq ggal/liver_2.sfq
-rm ggal/liver*fq
+rm ggal/liver*.fq
 rm ggal/lung_1.fq
 apptainer run docker://quay.io/biocontainers/kallisto:0.50.1--h6de1650_2 kallisto index -t 4 --index ggal/kallisto.idx ggal/transcriptome.fa
 wget https://genome-idx.s3.amazonaws.com/kraken/k2_minusb_20240112.tar.gz
